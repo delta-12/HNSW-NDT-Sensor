90599bf5622000958d3fe315a15aeb9206d5dec0
delta-12 <btesq235@gmail.com>
2026-05-13 16:32:14 -0400

    WIP: configure timers and UART

--- boards/BlackPill/bsp_user/src/bsp_user.c
+++ boards/BlackPill/bsp_user/src/bsp_user.c
@@ -1,5 +1,6 @@
 #include "bsp_user.h"
 
+#include "dma.h"
 #include "gpio.h"
 #include "tim.h"
 #include "usart.h"
@@ -7,7 +8,9 @@
 void BspUser_Initialize(void)
 {
     MX_GPIO_Init();
+    MX_DMA_Init();
     MX_USART1_UART_Init();
-    MX_TIM1_Init();
-    MX_TIM2_Init();
+    MX_USART2_UART_Init();
+    MX_TIM10_Init();
+    MX_TIM11_Init();
 }
