bf321d293d7833943887c22c19f6f98047c9be2f
delta-12 <btesq235@gmail.com>
2026-05-28 11:39:19 -0400

    add logging

--- sensor/src/main.c
+++ sensor/src/main.c
@@ -5,6 +5,7 @@
 #include "bsp.h"
 #include "bsp_gpio.h"
 #include "bsp_gpio_user.h"
+#include "bsp_logger.h"
 #include "bsp_uart.h"
 #include "bsp_uart_user.h"
 
@@ -33,10 +34,11 @@ typedef enum
     SERIAL_COMMAND_STEPPER_RAISE = 0x04U
 } Serial_Command_t;
 
-static uint32_t        samples[SAMPLE_SIZE];
-static uint8_t         serial_buffer[4096U];
-static uint8_t         serial_encoding_buffer[sizeof(serial_buffer)];
-static A9488_Context_t stepper_motor = {
+static const char *const log_tag = "MAIN";
+static uint32_t          samples[SAMPLE_SIZE];
+static uint8_t           serial_buffer[4096U];
+static uint8_t           serial_encoding_buffer[sizeof(serial_buffer)];
+static A9488_Context_t   stepper_motor = {
     .timer       = BSP_TIMER_USER_TIMER_STEPPER,
     .step        = BSP_GPIO_USER_PIN_STEPPER_MOTOR_STEP,
     .direction   = BSP_GPIO_USER_PIN_STEPPER_MOTOR_DIRECTION,
@@ -53,6 +55,8 @@ int main(void)
 
     A4988_Initialize(&stepper_motor);
     Sensor_Initialize();
+
+    BSP_LOGGER_LOG_INFO(log_tag, "Initialized");
 
     while (true)
     {
@@ -82,9 +86,12 @@ static void Task(void)
     if (sizeof(serial_buffer) == received)
     {
         /* TODO */
+        BSP_LOGGER_LOG_WARNING(log_tag, "Packet exceeds buffer size");
     }
     else if (BSP_ERROR_NONE == error)
     {
+        BSP_LOGGER_LOG_DEBUG(log_tag, "Packet received");
+
         size_t size = Cobs_Decode(serial_encoding_buffer, sizeof(serial_encoding_buffer), serial_buffer, received);
 
         if ((SIZE_MAX != size) &&
@@ -93,12 +100,16 @@ static void Task(void)
         {
             if (SERIAL_COMMAND_SAMPLE_START == serial_encoding_buffer[SERIAL_BYTE_COMMAND])
             {
+                BSP_LOGGER_LOG_DEBUG(log_tag, "Sampling...");
+
                 (void)BspGpio_Write(BSP_GPIO_USER_PIN_LED, BSP_GPIO_STATE_RESET);
                 Sensor_Sample(samples, SAMPLE_SIZE, NULL);
                 while (Sensor_IsSampling())
                 {
                 }
                 Sensor_ConvertRawSamples(samples, SAMPLE_SIZE);
+
+                BSP_LOGGER_LOG_DEBUG(log_tag, "Sampling complete");
 
                 serial_encoding_buffer[SERIAL_BYTE_COMMAND] = SERIAL_COMMAND_SAMPLE_COMPLETE;
                 for (size_t i = SERIAL_BYTE_MAX; i < SAMPLE_SIZE + SERIAL_BYTE_MAX; i++)
@@ -117,24 +128,40 @@ static void Task(void)
                 }
                 else
                 {
+                    BSP_LOGGER_LOG_ERROR(log_tag, "Link error");
+
                     (void)BspUart_Stop(BSP_UART_USER_1);
                     (void)BspUart_Start(BSP_UART_USER_1);
                 }
             }
             else if (SERIAL_COMMAND_STEPPER_LOWER == serial_encoding_buffer[SERIAL_BYTE_COMMAND])
             {
+                BSP_LOGGER_LOG_DEBUG(log_tag, "Lowering sensor...");
+
                 (void)A4988_Move(&stepper_motor, -5 * 200 * 16);
                 Bsp_Delay(3000);
+
+                BSP_LOGGER_LOG_DEBUG(log_tag, "Sensor lowered");
             }
             else if (SERIAL_COMMAND_STEPPER_RAISE == serial_encoding_buffer[SERIAL_BYTE_COMMAND])
             {
+                BSP_LOGGER_LOG_DEBUG(log_tag, "Raising sensor...");
+
                 (void)A4988_Move(&stepper_motor, 5 * 200 * 16);
                 Bsp_Delay(3000);
+
+                BSP_LOGGER_LOG_DEBUG(log_tag, "Sensor raised");
+            }
+            else
+            {
+                BSP_LOGGER_LOG_WARNING(log_tag, "Invalid command");
             }
         }
     }
     else
     {
+        BSP_LOGGER_LOG_ERROR(log_tag, "Link error");
+
         (void)BspUart_Stop(BSP_UART_USER_1);
         (void)BspUart_Start(BSP_UART_USER_1);
     }
